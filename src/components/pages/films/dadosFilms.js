3b07887a6cdaaed1560486f781cea37c6b3366d8
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-12-10 19:52:31 -0300

    Baixando imagens a serem usadas e criando dados estáticos para fazer os cartões.

---
+++ src/components/pages/films/dadosFilms.js
@@ -0,0 +1,65 @@
+import img1 from '../../../assets/imgs/film/img1.jpg'
+import img2 from '../../../assets/imgs/film/img2.jpg'
+import img3 from '../../../assets/imgs/film/img3.jpg'
+import img4 from '../../../assets/imgs/film/img4.jpg'
+import img5 from '../../../assets/imgs/film/img5.jpg'
+import img6 from '../../../assets/imgs/film/img6.jpg'
+import img7 from '../../../assets/imgs/film/img7.jpeg'
+import img8 from '../../../assets/imgs/film/img8.jpg'
+import img9 from '../../../assets/imgs/film/img9.jpg'
+import img10 from '../../../assets/imgs/film/img10.jpg'
+import img11 from '../../../assets/imgs/film/img11.jpg'
+import img12 from '../../../assets/imgs/film/img12.jpg'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    { 
+        name: "Capitã Marvel", 
+        img: img1, 
+        sinopse: "Em Capitã Marvel, Carol Danvers (Brie Larson) é uma ex-agente da Força Aérea norte-americana, que, sem se lembrar de sua vida na Terra, é recrutada pelos Kree para fazer parte de seu exército de elite. Inimiga declarada dos Skrull, ela acaba voltando ao seu planeta de origem para impedir uma invasão dos metaformos, e assim vai acabar descobrindo a verdade sobre si, com a ajuda do agente Nick Fury (Samuel L. Jackson) e da gata Goose."
+    }, { 
+        name: "Pantera Negra", 
+        img: img2, 
+        sinopse: "Em Pantera Negra, após a morte do rei T'Chaka (John Kani), o príncipe T'Challa (Chadwick Boseman) retorna a Wakanda para a cerimônia de coroação. Nela são reunidas as cinco tribos que compõem o reino, sendo que uma delas, os Jabari, não apoia o atual governo. T'Challa logo recebe o apoio de Okoye (Danai Gurira), a chefe da guarda de Wakanda, da irmã Shuri (Letitia Wright), que coordena a área tecnológica do reino, e também de Nakia (Lupita Nyong'o), a grande paixão do atual Pantera Negra, que não quer se tornar rainha. Juntos, eles estão à procura de Ulysses Klaue (Andy Serkis), que roubou de Wakanda um punhado de vibranium, alguns anos atrás."
+    }, { 
+        name: "Thor: Ragnarok", 
+        img: img3, 
+        sinopse: "Em Thor: Ragnarok, Thor (Chris Hemsworth) está preso do outro lado do universo. Ele precisa correr contra o tempo para voltar a Asgard e impedir o Ragnarok, a destruição de seu mundo, que está nas mãos da poderosa e implacável vilã Hela (Cate Blanchett)."
+    }, { 
+        name: "Guadiões da Galáxia Vol. 2", 
+        img: img4, 
+        sinopse: "Agora já conhecidos como os Guardiões da Galáxia, os guerreiros viajam ao longo do cosmos e lutam para manter sua nova família unida. Enquanto isso tentam desvendar os mistérios da verdadeira paternidade de Peter Quill (Chris Pratt)."
+    }, { 
+        name: "Doutor Estranho", 
+        img: img5, 
+        sinopse: "Stephen Strange (Benedict Cumberbatch) leva uma vida bem sucedida como neurocirurgião. Sua vida muda completamente quando sofre um acidente de carro e fica com as mãos debilitadas. Devido a falhas da medicina tradicional, ele parte para um lugar inesperado em busca de cura e esperança, um misterioso enclave chamado Kamar-Taj, localizado em Katmandu. Lá descobre que o local não é apenas um centro medicinal, mas também a linha de frente contra forças malignas místicas que desejam destruir nossa realidade. Ele passa a treinar e adquire poderes mágicos, mas precisa decidir se vai voltar para sua vida comum ou defender o mundo."
+    }, { 
+        name: "Capitão América 2 - O Soldado Invernal", 
+        img: img6, 
+        sinopse: "Dois anos após os acontecimentos em Nova York (Os Vingadores - The Avengers), Steve Rogers (Chris Evans) continua seu dedicado trabalho com a agência S.H.I.E.L.D. e também segue tentando se acostumar com o fato de que foi descongelado e acordou décadas depois de seu tempo. Em parceria com Natasha Romanoff (Scarlett Johansson), também conhecida como Viúva Negra, ele é obrigado a enfrentar um poderoso e misterioso inimigo chamado Soldado Invernal, que visita Washington e abala o dia a dia da S.H.I.E.L.D., ainda liderada por Nick Fury (Samuel L. Jackson)."
+    }, { 
+        name: "Shang-Chi e a Lenda dos Dez Anéis", 
+        img: img7, 
+        sinopse: "Em Shang-Chi e a Lenda dos Dez Anéis, acompanhamos a história de Shang-Chi (Simu Liu), um jovem chinês que foi criado por seu pai em reclusão para que pudesse focar totalmente em ser um mestre de artes marciais. Entretanto, quando ele tem a chance de entrar em contato com o resto do mundo pela primeira vez, logo percebe que seu pai não é o humanitário que dizia ser, vendo-se obrigado a se rebelar e traçar o seu próprio caminho."
+    }, { 
+        name: "Eternos", 
+        img: img8, 
+        sinopse: "Eternos são seres super dotados com características como imortalidade e manipulação de energia cósmica, eles são frutos de experiências fracassadas de seus próprio criador, o Celestial Arishem, desde a criação da Terra há milhões de anos. Destinados a salvar o mundo e a raça humana dos Deviantes, seres também criados pelo Celestial, os 5 Eternos então derrotam tais seres e seguem caminhos diferentes, esperando que seu criador volta com boas novas. Mas após séculos e milênios aguardando ele, o grupo de heróis agora precisam se preparar para uma nova ameaça."
+    }, { 
+        name: "Homem-Aranha: Longe de Casa", 
+        img: img9, 
+        sinopse: "Em Homem-Aranha: Longe de Casa, Peter Parker (Tom Holland) está em uma viagem de duas semanas pela Europa, ao lado de seus amigos de colégio, quando é surpreendido pela visita de Nick Fury (Samuel L. Jackson). Precisando de ajuda para enfrentar monstros nomeados como Elementais, Fury o convoca para lutar ao lado de Mysterio (Jake Gyllenhaal), um novo herói que afirma ter vindo de uma Terra paralela. Além da nova ameaça, Peter precisa lidar com a lacuna deixada por Tony Stark, que deixou para si seu óculos pessoal, com acesso a um sistema de inteligência artificial associado à Stark Industries."
+    }, { 
+        name: "Guardiões Da Galáxia", 
+        img: img10, 
+        sinopse: "Peter Quill (Chris Pratt) foi abduzido da Terra quando ainda era criança. Adulto, fez carreira como saqueador e ganhou o nome de Senhor das Estrelas. Quando rouba uma esfera, na qual o poderoso vilão Ronan, da raça kree, está interessado, passa a ser procurado por vários caçadores de recompensas. Para escapar do perigo, Quill une forças com quatro personagens fora do sistema: Groot, uma árvore humanóide (Vin Diesel), a sombria e perigosa Gamora (Zoe Saldana), o guaxinim rápido no gatilho Rocket Racoon (Bradley Cooper) e o vingativo Drax, o Destruidor (Dave Bautista). Mas o Senhor das Estrelas descobre que a esfera roubada possui um poder capaz de mudar os rumos do universo, e logo o grupo deverá proteger o objeto para salvar o futuro da galáxia."
+    }, { 
+        name: "Homem de Ferro 3", 
+        img: img11, 
+        sinopse: "Desde o ataque dos chitauri a Nova York, Tony Stark (Robert Downey Jr.) vem enfrentando dificuldades para dormir e, quando consegue, tem terríveis pesadelos. Ele teme não conseguir proteger sua namorada Pepper Potts (Gwyneth Paltrow) dos vários inimigos que passou a ter após vestir a armadura do Homem de Ferro. Um deles, o Mandarim (Ben Kingsley), decide atacá-lo com força total, destruindo sua mansão e colocando a vida de Pepper em risco. Para enfrentá-lo Stark precisará ressurgir do fundo do mar, para onde foi levado junto com os destroços da mansão, e superar seu maior medo: o de fracassar."
+    }, { 
+        name: "Homem-Formiga e a Vespa", 
+        img: img12, 
+        sinopse: "Após ter ajudado o Capitão América na batalha contra o Homem de Ferro na Alemanha, Scott Lang (Paul Rudd) é condenado a dois anos de prisão domiciliar, por ter quebrado o Tratado de Sokovia. Diante desta situação, ele foi obrigado a se aposentar temporariamente do posto de super-herói. Restando apenas três dias para o término deste prazo, ele tem um estranho sonho com Janet Van Dyne (Michelle Pfeiffer), que desapareceu 30 anos atrás ao entrar no mundo quântico em um ato de heroísmo. Ao procurar o dr. Hank Pym (Michael Douglas) e sua filha Hope (Evangeline Lilly) em busca de explicações, Scott é rapidamente cooptado pela dupla para que possa ajudá-los em sua nova missão: construir um túnel quântico, com o objetivo de resgatar Janet de seu limbo."
+    },
+]
---
+++ src/components/pages/gallery/imagens.js
@@ -0,0 +1,28 @@
+import img1 from '../../../assets/imgs/gallery/img1.jpg'
+import img2 from '../../../assets/imgs/gallery/img2.jpg'
+import img3 from '../../../assets/imgs/gallery/img3.jpg'
+import img4 from '../../../assets/imgs/gallery/img4.jpg'
+import img5 from '../../../assets/imgs/gallery/img5.jpg'
+import img6 from '../../../assets/imgs/gallery/img6.png'
+import img7 from '../../../assets/imgs/gallery/img7.jpg'
+import img8 from '../../../assets/imgs/gallery/img8.jpg'
+import img9 from '../../../assets/imgs/gallery/img9.jpg'
+import img10 from '../../../assets/imgs/gallery/img10.jpg'
+import img11 from '../../../assets/imgs/gallery/img11.jpg'
+import img12 from '../../../assets/imgs/gallery/img12.jpg'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    { imagem: img1, description: '' },
+    { imagem: img2, description: '' },
+    { imagem: img3, description: '' },
+    { imagem: img4, description: '' },
+    { imagem: img5, description: '' },
+    { imagem: img6, description: '' },
+    { imagem: img7, description: '' },
+    { imagem: img8, description: '' }, 
+    { imagem: img9, description: '' }, 
+    { imagem: img10, description: '' }, 
+    { imagem: img11, description: '' }, 
+    { imagem: img12, description: '' }
+]
