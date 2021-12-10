15eb7500e0a0ce464ffe783a7b1d024235f8109e
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-12-10 19:55:58 -0300

    Fazendo o footer.

---
+++ src/components/layout/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import './footer.css'
+
+export default function Footer(props){
+    return(
+        <footer className='footer'>
+            <p>Desenvolvidor por Odair</p>
+            <p>Contato: <strong>odairferreira97@gmail.com</strong></p>
+        </footer>
+    )
+}
